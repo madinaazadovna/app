869ea347a49f551dde20ade893dd65d2e4f45f43
Mukanova Madina <m_mukanova@kbtu.kz>
2023-01-23 20:46:39 +0600

    first commit

---
+++ 10_tuples.py
@@ -0,0 +1,17 @@
+#ex1
+fruits = ("apple", "banana", "cherry")
+print(fruits[0])
+
+#ex2
+fruits = ("apple", "banana", "cherry")
+print(len(fruits))
+
+#ex3
+fruits = ("apple", "banana", "cherry")
+print(fruits[-1])
+
+#ex4
+fruits = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(fruits[3:6])
+
+#ex5
