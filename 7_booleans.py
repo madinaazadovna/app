29a676841b34fcf27b68bddff2b3181449aa545d
Mukanova Madina <m_mukanova@kbtu.kz>
2023-01-23 20:25:28 +0600

    first commit

---
+++ 7_booleans.py
@@ -0,0 +1,19 @@
+#ex1
+print(10>9)
+answer: True
+
+#ex2
+print(10==9)
+answer: False
+
+#ex3
+print(10<9)
+answer: False
+
+#ex4
+print(bool("abc"))
+answer: True
+
+#ex5
+print(bool(0))
+answer: True
