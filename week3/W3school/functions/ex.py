354a27ec7cae5e57f7c1d983bd2f66f52570b650
Mukanova Madina <m_mukanova@kbtu.kz>
2023-02-12 00:07:26 +0600

    .

---
+++ week3/W3school/functions/ex.py
@@ -0,0 +1,81 @@
+#In Python a function is defined using the def keyword:
+def my_function():
+  print("Hello from a function")
+
+#Information can be passed into functions as arguments.
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+
+#This function expects 2 arguments, and gets 2 arguments:
+def my_function(fname, iname):
+  print(fname + " " + iname)
+my_function("Emil", "Refsnes")
+
+#If you try to call the function with 1 or 3 arguments, you will get an error:
+#This function expects 2 arguments, but gets only 1:
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil")
+
+#If the number of arguments is unknown, add a * before the parameter name:
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+
+my_function("Emil", "Tobias", "Linus")
+
+#You can also send arguments with the key = value syntax.
+def my_function(child3, child2, child1):
+  print("The youngest child is " + child3)
+
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+
+#If the number of keyword arguments is unknown, add a double ** before the parameter name:
+def my_function(**kid):
+  print("His last name is " + kid["lname"])
+
+my_function(fname = "Tobias", lname = "Refsnes")
+
+#If we call the function without argument, it uses the default value:
+def my_function(country = "Norway"):
+  print("I am from " + country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
+
+#Passing a List as an Argument
+def my_function(food):
+  for i in food:
+    print(i)
+fruits = ["apple", "banana", "cherry"]
+my_function(fruits)
+
+#To let a function return a value, use the return statement:
+def my_function(x):
+  return 5 * x
+
+print(my_function(3))
+print(my_function(5))
+print(my_function(9))
+
+#function definitions cannot be empty, but if you for some reason have a function definition with no content, put in the pass statement to avoid getting an error.
+def myfunction():
+  pass
+
+#Recursion
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("\n\nRecursion Example Results")
+tri_recursion(6)
